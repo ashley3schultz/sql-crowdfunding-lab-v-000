000a661cb6cd54ff2782f5d4d05a9d12886d8e8c
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-01 00:51:42 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT users.name, SUM(pledges.amount) FROM users JOIN pledges ON pledges.user_id = users.id GROUP BY users.name ORDER BY users.name, SUM(pledges.amount)"
+"SELECT users.name, SUM(pledges.amount) FROM users JOIN pledges ON pledges.user_id = users.id GROUP BY users.name ORDER BY SUM(pledges.amount), users.name"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
@@ -29,3 +29,8 @@ end
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
 "Write your SQL query Here"
 end
+
+[["Iguana", 10], ["Swizzle", 12], ["Sirius", 19], ["Alex", 20], ["Marisa", 24], ["Voldemort", 34], ["Amanda", 40], ["Bear", 50], ["Hermione", 50], ["Rosey", 50], ["Pacha", 60], ["Sophie", 60], ["
+Finnebar", 70], ["Franz", 90], ["Ena", 100], ["Whale", 125.5], ["Katie", 170], ["Squid", 270], ["Albus", 470], ["Victoria", 1700]]
+[["Albus", 470], ["Alex", 20], ["Amanda", 40], ["Bear", 50], ["Ena", 100], ["Finnebar", 70], ["Franz", 90], ["Hermione", 50], ["Iguana", 10], ["Katie", 170], ["Marisa", 24], ["Pacha", 60], ["Rose
+y", 50], ["Sirius", 19], ["Sophie", 60], ["Squid", 270], ["Swizzle", 12], ["Victoria", 1700], ["Voldemort", 34], ["Whale", 125.5]]
