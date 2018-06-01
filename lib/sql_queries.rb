9fb7ebd90a1c04e90eef62cecd94c7f590f7db95
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-01 00:51:53 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -29,8 +29,3 @@ end
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
 "Write your SQL query Here"
 end
-
-[["Iguana", 10], ["Swizzle", 12], ["Sirius", 19], ["Alex", 20], ["Marisa", 24], ["Voldemort", 34], ["Amanda", 40], ["Bear", 50], ["Hermione", 50], ["Rosey", 50], ["Pacha", 60], ["Sophie", 60], ["
-Finnebar", 70], ["Franz", 90], ["Ena", 100], ["Whale", 125.5], ["Katie", 170], ["Squid", 270], ["Albus", 470], ["Victoria", 1700]]
-[["Albus", 470], ["Alex", 20], ["Amanda", 40], ["Bear", 50], ["Ena", 100], ["Finnebar", 70], ["Franz", 90], ["Hermione", 50], ["Iguana", 10], ["Katie", 170], ["Marisa", 24], ["Pacha", 60], ["Rose
-y", 50], ["Sirius", 19], ["Sophie", 60], ["Squid", 270], ["Swizzle", 12], ["Victoria", 1700], ["Voldemort", 34], ["Whale", 125.5]]
